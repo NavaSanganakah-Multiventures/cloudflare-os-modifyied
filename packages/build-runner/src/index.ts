7ffed6abc4bd71d103a7caf960a66e1f23101e77
navasanganakah <navasanganakah@gmail.com>
2026-08-18 10:30:43 +0530

    Remove unused DurableObject import from build-runner

--- packages/build-runner/src/index.ts
+++ packages/build-runner/src/index.ts
@@ -1,5 +1,5 @@
 import { Container, getContainer } from "@cloudflare/containers";
-import { DurableObject, DurableObjectNamespace, WorkerEntrypoint } from "cloudflare:workers";
+import { DurableObjectNamespace, WorkerEntrypoint } from "cloudflare:workers";
 
 export interface BuildRequest {
   /** HTTPS URL used to clone the repository. For private repos, embed an access token. */
