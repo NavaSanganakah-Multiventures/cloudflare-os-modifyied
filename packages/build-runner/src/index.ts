69cd75bcc948df7a5d6acf50af7c3b9bc0fff303
Dheerendra Tripathi <153520649+acharypdt@users.noreply.github.com>
2026-08-21 07:42:10 +0530

    build-runner: remove DurableObjectNamespace import from cloudflare:workers

--- packages/build-runner/src/index.ts
+++ packages/build-runner/src/index.ts
@@ -1,5 +1,5 @@
 import { Container, getContainer } from "@cloudflare/containers";
-import { DurableObjectNamespace, WorkerEntrypoint } from "cloudflare:workers";
+import { WorkerEntrypoint } from "cloudflare:workers";
 
 export interface BuildRequest {
   /** HTTPS URL used to clone the repository. For private repos, embed an access token. */
